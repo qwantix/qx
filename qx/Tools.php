63b08d6934af8602aacc46b1a4673637007be3ca
Brice Dauzats <qwantix@gmail.com>
2013-06-09 00:55:13 +0200

    Add generate password method

--- qx/Tools.php
+++ qx/Tools.php
@@ -229,5 +229,32 @@ class Tools
 	{
 		return mb_check_encoding($value,"UTF-8") ? utf8_decode($value) : $value;
 	}
+
+	static public function GeneratePassword($length=8, $strength=4)
+	{
+		$vowels = 'aeuyo';
+		$consonants = 'bdghjmnpqrstvz';
+		if ($strength & 1)
+			$consonants .= 'BDGHJLMNPQRSTVWXZ';
+		if ($strength & 2)
+			$vowels .= "AEUY";
+		if ($strength & 4)
+			$consonants .= '23456789';
+		if ($strength & 8)
+			$consonants .= '@#$%';
+
+		$password = '';
+		$alt = time() % 2;
+		for ($i = 0; $i < $length; $i++) {
+			if ($alt == 1) {
+				$password .= $consonants[(rand() % strlen($consonants))];
+				$alt = 0;
+			} else {
+				$password .= $vowels[(rand() % strlen($vowels))];
+				$alt = 1;
+			}
+		}
+		return $password;
+	}
 }
 
