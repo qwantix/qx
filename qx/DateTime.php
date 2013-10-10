d878a5852c98985dc0d0447e9b826f28c891228b
Brice Dauzats <qwantix@gmail.com>
2013-10-10 08:28:20 +0200

    Add isEmpty method

--- qx/DateTime.php
+++ qx/DateTime.php
@@ -7,4 +7,12 @@ class DateTime extends \DateTime
 	{
 		return \DateTime::createFromFormat( __('@date_format') , $value);
 	}
+	static public function IsEmpty($value)
+	{
+		if(is_string($value))
+			return !preg_match('`[1-9]`', $value);
+		else if($value instanceof \DateTime)
+			return $value->getTimestamp() == 0;
+		return empty($value);
+	}
 }
